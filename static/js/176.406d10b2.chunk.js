2eb4dac046374f6d7418a9e631865ea7598e5541
Valentin Mendoza <jmendozat13@gmail.com>
2024-10-23 10:20:44 -0500

    Updates

---
+++ static/js/176.406d10b2.chunk.js
@@ -0,0 +1,94 @@
+"use strict";(self.webpackChunkyalisto_landing=self.webpackChunkyalisto_landing||[]).push([[176],{5639:(e,i,t)=>{t.d(i,{$:()=>a});const n=(0,t(9).Ay)("button")`
+  background: ${e=>e.color||"#EC4A0A"};
+  color: ${e=>e.color?"#EC4A0A":"#fff"};
+  font-size: 1rem;
+  font-weight: 700;
+  width: 100%;
+  border: 1px solid #F5FAFF;
+  border-radius: 4px;
+  padding: 13px 0;
+  cursor: pointer;
+  margin-top: 0.625rem;
+  max-width: ${e=>e.maxWidth||"180px"};
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0 16px 30px rgb(23 31 114 / 20%);
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: #fff;
+    border: 1px solid rgb(255, 133, 58);
+    background-color: rgb(255, 133, 58);
+  }
+`;var s=t(579);const a=e=>{let{color:i,maxWidth:t,children:a,onClick:o}=e;return(0,s.jsx)(n,{color:i,maxWidth:t,onClick:o,children:a})}},8176:(e,i,t)=>{t.r(i),t.d(i,{default:()=>F});var n=t(5043);const s=JSON.parse('{"DD":"Digitaliza el men\xfa de tu restaurante.","Qq":"Transforma la experiencia de tus clientes con un men\xfa digital. Ofrece una navegaci\xf3n f\xe1cil y r\xe1pida desde sus dispositivos m\xf3viles, mant\xe9n tu oferta siempre actualizada.","x6":[{"title":"Explorar","deeplink":"https://play.google.com/store/apps/details?id=com.afoxplus.yalisto&pcampaignid=web_share"},{"title":"M\xe1s Informaci\xf3n","color":"#fff","deeplink":"https://wa.me/+51966997499"}]}'),a=JSON.parse('{"DD":"Publicar tu Establecimiento en YaListo es Muy F\xe1cil","Qq":"Registra los datos de tu establecimiento y te contactaremos para cargar tu men\xfa. \xa1\xdanete a YaListo y comienza a atraer m\xe1s clientes en un instante!","x6":{"title":"Publica tu establecimiento","deeplink":"https://forms.gle/GUBmE6YyndHkyE2R7"}}'),o=JSON.parse('{"DD":"Somos una Startup Innovadora","Qq":"En YaListo, atraemos clientes a tu establecimiento, mejoramos la experiencia de visualizaci\xf3n de tu men\xfa y aceleramos el proceso de pedidos. \xa1\xdanete a nosotros y transforma tu negocio!","uW":[{"title":"\xbfPor qu\xe9 Elegir YaListo?","content":"La digitalizaci\xf3n es un aliado clave para tu negocio. Con YaListo, aumenta tu visibilidad y potencia tus ventas de manera efectiva.","icon":"notes.svg"}]}'),r=JSON.parse('{"D":"Conectar Sabores y Recuerdos","Q":"Nuestra misi\xf3n es facilitar que los clientes descubran restaurantes acogedores y deliciosos, donde puedan disfrutar de momentos inolvidables en familia."}'),l=JSON.parse('{"D":"Segura, R\xe1pida e Intuitiva","Q":"En YaListo, protegemos tus datos y los de tus clientes. Nuestra aplicaci\xf3n es r\xe1pida y f\xe1cil de usar, dise\xf1ada para que disfrutes de una experiencia sin complicaciones."}');var c=t(6054),d=t(1319),p=t(3660),u=t(4647),x=t(9281),m=t(1766),h=t(2646),g=t(5639),y=t(9),b=t(2582),f=t(1919);const j=(0,y.Ay)("header")`
+  padding: 1rem 0.5rem;
+
+  .ant-row-space-between {
+    align-items: center;
+    text-align: center;
+  }
+`,v=(0,y.Ay)(b.N_)`
+  display: flex;
+`,w=(0,y.Ay)("div")`
+  display: inline-block;
+  text-align: center;
+`,A=((0,y.Ay)("div")`
+  width: 203px;
+  display: inline-block;
+
+  @media only screen and (max-width: 411px) {
+    width: 150px;
+  }
+
+  @media only screen and (max-width: 320px) {
+    width: 118px;
+  }
+`,(0,y.Ay)("div")`
+  @media only screen and (max-width: 890px) {
+    display: block;
+  }
+
+  display: none;
+
+  svg {
+    fill: #2e186a;
+  }
+`),k=(0,y.Ay)("div")`
+  @media only screen and (max-width: 890px) {
+    display: none;
+  }
+`,D=(0,y.Ay)("h5")`
+  font-size: 1.5rem;
+  font-weight: 600;
+  text-align: center;
+`,C=(0,y.Ay)(w)`
+  font-size: 1.2rem;
+  color: #101323;
+  transition: color 0.2s ease-in;
+  margin: 0.5rem 2rem;
+
+  @media only screen and (max-width: 768px) {
+    margin: 1.25rem 2rem;
+  }
+`,z=(0,y.Ay)("span")`
+  font-weight: 500;
+  color: #404041;
+  text-align: right;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`,E=(0,y.Ay)(f.A)`
+  font-size: 22px;
+`,Q=(0,y.Ay)("span")`
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: rgb(255, 130, 92);
+    text-underline-position: under;
+    text-decoration: rgb(255, 130, 92) wavy underline;
+  }
+`;var S=t(579);const q=(0,x.C)()((e=>{let{t:i}=e;const[t,s]=(0,n.useState)(!1),a=()=>{s(!t)},o=()=>{const e=e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"}),s(!1)};return(0,S.jsxs)(S.Fragment,{children:[(0,S.jsx)(C,{onClick:()=>e("about"),children:(0,S.jsx)(Q,{children:i("About")})}),(0,S.jsx)(C,{onClick:()=>e("mission"),children:(0,S.jsx)(Q,{children:i("Mission")})}),(0,S.jsx)(C,{onClick:()=>e("product"),children:(0,S.jsx)(Q,{children:i("Product")})}),(0,S.jsx)(C,{style:{width:"180px"},onClick:()=>e("contact"),children:(0,S.jsx)(Q,{children:(0,S.jsx)(g.$,{children:i("Contact")})})})]})};return(0,S.jsx)(j,{children:(0,S.jsxs)(m.default,{children:[(0,S.jsxs)(d.A,{justify:"space-between",children:[(0,S.jsx)(v,{to:"/","aria-label":"homepage",children:(0,S.jsx)(h.A,{src:"logo.svg",width:"101px",height:"64px"})}),(0,S.jsx)(k,{children:(0,S.jsx)(o,{})}),(0,S.jsx)(A,{onClick:a,children:(0,S.jsx)(E,{})})]}),(0,S.jsxs)(p.A,{closable:!1,open:t,onClose:a,children:[(0,S.jsx)(u.A,{style:{marginBottom:"2.5rem"},children:(0,S.jsxs)(z,{onClick:a,children:[(0,S.jsx)(u.A,{span:12,children:(0,S.jsx)(D,{children:"Menu"})}),(0,S.jsx)(u.A,{span:12,children:(0,S.jsx)(E,{})})]})}),(0,S.jsx)(o,{})]})]})})})),N=(0,n.lazy)((()=>Promise.all([t.e(19),t.e(545),t.e(676)]).then(t.bind(t,1676)))),P=(0,n.lazy)((()=>Promise.all([t.e(19),t.e(416)]).then(t.bind(t,9416)))),L=(0,n.lazy)((()=>Promise.resolve().then(t.bind(t,1766)))),O=(0,n.lazy)((()=>t.e(538).then(t.bind(t,8538)))),Y=(0,n.lazy)((()=>Promise.all([t.e(19),t.e(566)]).then(t.bind(t,6566)))),F=()=>(0,S.jsxs)(L,{children:[(0,S.jsx)(q,{}),(0,S.jsx)(O,{}),(0,S.jsx)(Y,{direction:"right",title:s.DD,content:s.Qq,button:s.x6,icon:"developer.svg",id:"intro"}),(0,S.jsx)(P,{title:a.DD,content:a.Qq,button:a.x6}),(0,S.jsx)(Y,{direction:"left",title:o.DD,content:o.Qq,section:o.uW,icon:"graphs.svg",id:"about"}),(0,S.jsx)(Y,{direction:"right",title:r.D,content:r.Q,icon:"product-launch.svg",id:"mission"}),(0,S.jsx)(Y,{direction:"left",title:l.D,content:l.Q,icon:"waving.svg",id:"product"}),(0,S.jsx)(N,{title:c.D,content:c.Q,id:"contact"})]})},6054:e=>{e.exports=JSON.parse('{"D":"Cont\xe1ctanos","Q":"\xbfEst\xe1s listo para llevar tu restaurante al siguiente nivel? \xa1Escr\xedbenos! Estaremos encantados de atenderte lo m\xe1s pronto posible."}')}}]);
+//# sourceMappingURL=176.406d10b2.chunk.js.map
---
+++ static/js/416.772abc33.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunkyalisto_landing=self.webpackChunkyalisto_landing||[]).push([[416],{9416:(e,n,i)=>{i.r(n),i.d(n,{default:()=>o});var t=i(1319),d=i(4647),r=i(9281),l=i(4019),a=i(5639),s=i(9);const c=(0,s.Ay)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,x=(0,s.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,m=(0,s.Ay)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;var h=i(579);const o=(0,r.C)()((e=>{let{title:n,content:i,button:r,t:s}=e;return(0,h.jsx)(c,{children:(0,h.jsx)(l.q7,{direction:"up",triggerOnce:!0,children:(0,h.jsx)(t.A,{justify:"center",align:"middle",children:(0,h.jsx)(m,{children:(0,h.jsxs)(d.A,{lg:24,md:24,sm:24,xs:24,children:[(0,h.jsx)("h6",{children:s(n)}),(0,h.jsx)(x,{children:s(i)}),r&&(0,h.jsx)(a.$,{name:"submit",maxWidth:"226px",onClick:()=>{return e=r.deeplink,void window.open(e,"_blank");var e},children:s(r.title)})]})})})})})}))}}]);
+//# sourceMappingURL=416.772abc33.chunk.js.map
