81ba55ea7724f05e2de830ec3fe5ce6cd7204816
Valentin Mendoza <jmendozat13@gmail.com>
2025-07-02 11:25:49 -0500

    Updates

---
+++ static/js/176.7fb1413e.chunk.js
@@ -0,0 +1,94 @@
+"use strict";(self.webpackChunkyalisto_landing=self.webpackChunkyalisto_landing||[]).push([[176],{5639:(e,i,t)=>{t.d(i,{$:()=>s});const n=(0,t(9).Ay)("button")`
+  background: ${e=>e.color||"#EC4A0A"};
+  color: ${e=>e.color?"#EC4A0A":"#fff"};
+  font-size: 1rem;
+  font-weight: 700;
+  width: 100%;
+  border: 1px solid #F5FAFF;
+  border-radius: 4px;
+  padding: 13px 0;
+  cursor: pointer;
+  margin-top: 0.625rem;
+  max-width: ${e=>e.maxWidth||"180px"};
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0 16px 30px rgb(23 31 114 / 20%);
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: #fff;
+    border: 1px solid rgb(255, 133, 58);
+    background-color: rgb(255, 133, 58);
+  }
+`;var a=t(579);const s=e=>{let{color:i,maxWidth:t,children:s,onClick:o}=e;return(0,a.jsx)(n,{color:i,maxWidth:t,onClick:o,children:s})}},8176:(e,i,t)=>{t.r(i),t.d(i,{default:()=>O});var n=t(5043);const a=JSON.parse('{"DD":"Tu men\xfa inteligente con IA","Qq":"Ofrece tu men\xfa de forma interactiva y sorprende a tus clientes con una experiencia digital \xfanica. Con YaListo, digitaliza tu carta f\xe1cil y r\xe1pido.","x6":[{"title":"Explorar","deeplink":"https://play.google.com/store/apps/details?id=com.afoxplus.yalisto&pcampaignid=web_share"},{"title":"M\xe1s Informaci\xf3n","color":"#fff","deeplink":"https://wa.me/+51966997499"}]}'),s=JSON.parse('{"DD":"Publicar tu establecimiento es Muy F\xe1cil","Qq":"Registra tus datos y los de tu establecimiento... \xa1Y Listo! Empieza a mostrar tu men\xfa de una forma diferente, f\xe1cil y r\xe1pida. Con YaListo y la IA, cautiva a tus clientes desde el primer clic","x6":{"title":"Publica tu establecimiento","deeplink":"https://forms.gle/GUBmE6YyndHkyE2R7"}}'),o=JSON.parse('{"DD":"Somos una Startup Innovadora","Qq":"En YaListo, atraemos clientes a tu establecimiento, mejoramos la experiencia de visualizaci\xf3n de tu men\xfa y aceleramos el proceso de pedidos. \xa1\xdanete a nosotros y transforma tu negocio!","uW":[{"title":"\xbfPor qu\xe9 Elegir YaListo?","content":"La IA es un aliado clave para tu negocio. Con YaListo, aumenta tu visibilidad y potencia tus ventas de manera efectiva.","icon":"notes.svg"}]}'),r=JSON.parse('{"D":"Conectar Sabores y Recuerdos","Q":"Nuestra misi\xf3n es facilitar que los clientes descubran restaurantes acogedores y deliciosos, donde puedan disfrutar de momentos inolvidables en familia."}'),l=JSON.parse('{"D":"Segura, R\xe1pida e Intuitiva","Q":"En YaListo, protegemos tus datos y los de tus clientes. Nuestra aplicaci\xf3n es r\xe1pida y f\xe1cil de usar, dise\xf1ada para que disfrutes de una experiencia sin complicaciones."}');var c=t(1675),d=t(1319),u=t(3660),p=t(4647),x=t(9281),m=t(1766),h=t(2646),g=t(5639),y=t(9),b=t(2582),f=t(1919);const j=(0,y.Ay)("header")`
+  padding: 1rem 0.5rem;
+
+  .ant-row-space-between {
+    align-items: center;
+    text-align: center;
+  }
+`,v=(0,y.Ay)(b.N_)`
+  display: flex;
+`,A=(0,y.Ay)("div")`
+  display: inline-block;
+  text-align: center;
+`,w=((0,y.Ay)("div")`
+  width: 203px;
+  display: inline-block;
+
+  @media only screen and (max-width: 411px) {
+    width: 150px;
+  }
+
+  @media only screen and (max-width: 320px) {
+    width: 118px;
+  }
+`,(0,y.Ay)("div")`
+  @media only screen and (max-width: 890px) {
+    display: block;
+  }
+
+  display: none;
+
+  svg {
+    fill: #2e186a;
+  }
+`),k=(0,y.Ay)("div")`
+  @media only screen and (max-width: 890px) {
+    display: none;
+  }
+`,C=(0,y.Ay)("h5")`
+  font-size: 1.5rem;
+  font-weight: 600;
+  text-align: center;
+`,D=(0,y.Ay)(A)`
+  font-size: 1.2rem;
+  color: #101323;
+  transition: color 0.2s ease-in;
+  margin: 0.5rem 2rem;
+
+  @media only screen and (max-width: 768px) {
+    margin: 1.25rem 2rem;
+  }
+`,z=(0,y.Ay)("span")`
+  font-weight: 500;
+  color: #404041;
+  text-align: right;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`,Q=(0,y.Ay)(f.A)`
+  font-size: 22px;
+`,S=(0,y.Ay)("span")`
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: rgb(255, 130, 92);
+    text-underline-position: under;
+    text-decoration: rgb(255, 130, 92) wavy underline;
+  }
+`;var q=t(579);const E=(0,x.C)()((e=>{let{t:i}=e;const[t,a]=(0,n.useState)(!1),s=()=>{a(!t)},o=()=>{const e=e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"}),a(!1)};return(0,q.jsxs)(q.Fragment,{children:[(0,q.jsx)(D,{onClick:()=>e("about"),children:(0,q.jsx)(S,{children:i("About")})}),(0,q.jsx)(D,{onClick:()=>e("mission"),children:(0,q.jsx)(S,{children:i("Mission")})}),(0,q.jsx)(D,{onClick:()=>e("product"),children:(0,q.jsx)(S,{children:i("Product")})}),(0,q.jsx)(D,{style:{width:"180px"},onClick:()=>e("publish"),children:(0,q.jsx)(S,{children:(0,q.jsx)(g.$,{children:i("Publish")})})})]})};return(0,q.jsx)(j,{children:(0,q.jsxs)(m.default,{children:[(0,q.jsxs)(d.A,{justify:"space-between",children:[(0,q.jsx)(v,{to:"/","aria-label":"homepage",children:(0,q.jsx)(h.A,{src:"logo.svg",width:"101px",height:"64px"})}),(0,q.jsx)(k,{children:(0,q.jsx)(o,{})}),(0,q.jsx)(w,{onClick:s,children:(0,q.jsx)(Q,{})})]}),(0,q.jsxs)(u.A,{closable:!1,open:t,onClose:s,children:[(0,q.jsx)(p.A,{style:{marginBottom:"2.5rem"},children:(0,q.jsxs)(z,{onClick:s,children:[(0,q.jsx)(p.A,{span:12,children:(0,q.jsx)(C,{children:"Menu"})}),(0,q.jsx)(p.A,{span:12,children:(0,q.jsx)(Q,{})})]})}),(0,q.jsx)(o,{})]})]})})})),N=(0,n.lazy)((()=>Promise.all([t.e(19),t.e(545),t.e(537)]).then(t.bind(t,8537)))),P=(0,n.lazy)((()=>Promise.all([t.e(19),t.e(416)]).then(t.bind(t,9416)))),I=(0,n.lazy)((()=>Promise.resolve().then(t.bind(t,1766)))),L=(0,n.lazy)((()=>t.e(538).then(t.bind(t,8538)))),Y=(0,n.lazy)((()=>Promise.all([t.e(19),t.e(566)]).then(t.bind(t,6566)))),O=()=>(0,q.jsxs)(I,{children:[(0,q.jsx)(E,{}),(0,q.jsx)(L,{}),(0,q.jsx)(Y,{direction:"right",title:a.DD,content:a.Qq,button:a.x6,icon:"developer.svg",id:"intro"}),(0,q.jsx)(P,{title:s.DD,content:s.Qq,button:s.x6}),(0,q.jsx)(Y,{direction:"left",title:o.DD,content:o.Qq,section:o.uW,icon:"graphs.svg",id:"about"}),(0,q.jsx)(Y,{direction:"right",title:r.D,content:r.Q,icon:"product-launch.svg",id:"mission"}),(0,q.jsx)(Y,{direction:"left",title:l.D,content:l.Q,icon:"waving.svg",id:"product"}),(0,q.jsx)(N,{title:c.D,content:c.Q,id:"publish"})]})},1675:e=>{e.exports=JSON.parse('{"D":"\xa1Transforma tu negocio y hazlo crecer!","Q":"Reg\xedstrate ahora con tus datos y el de tu negocio para acceder a herramientas exclusivas que te ayudar\xe1n a llevar tu establecimiento al siguiente nivel. Recibir\xe1s tus accesos directamente en tu correo en breve."}')}}]);
+//# sourceMappingURL=176.7fb1413e.chunk.js.map
